709868e7567a8a6d54deaf82cd9be0b3bbce093a
Isabella <ikmaki14@gmail.com>
2018-12-03 12:02:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/isabellamaki/mp6/FirstFragment.java
@@ -0,0 +1,66 @@
+package com.example.isabellamaki.mp6;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment implements OnMapReadyCallback{
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+
+    public FirstFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.first_fragment, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502))
+                .title("Statue of Liberty").snippet("in New York"));
+        CameraPosition Liberty = CameraPosition.builder()
+                .target(new LatLng(40.689247, -74.044502)).zoom(16).bearing(0)
+                .tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+    }
+}
